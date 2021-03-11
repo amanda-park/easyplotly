fc00b733e804092edd73e76c737454e203d7e79d
Park <amanda.park@spectrumhealth.org>
2021-03-11 11:52:03 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,8 +2,11 @@ import plotly.express as px
 
 def clean_varname(df, var):
     """Formats text of variables so that titles and axes on Plotly graphs look cleaner.
+    
     :param Dataframe df: Required. A Pandas dataframe for plotting the data. 
+    
     :param str var: Required. The name of a variable in the data frame you want to clean the name of.
+    
     :returns: var_clean, df_clean
     """
     var_clean = var.replace("_", " ")
